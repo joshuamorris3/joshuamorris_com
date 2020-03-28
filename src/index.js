e3365fdfef37cc7bb0779fbe2521497d487aa7f9
joshuamorris3 <joshuamorris3@penguin>
2020-03-28 20:07:14 +0100

    Add list of icons for social links

--- src/index.js
+++ src/index.js
@@ -1,5 +1,10 @@
 import $ from "jquery";
 import 'normalize.css/normalize.css';
+import { library, dom } from '@fortawesome/fontawesome-svg-core';
+import { fab } from '@fortawesome/free-brands-svg-icons';
 import './js/main';
 import './js/plugins';
 import './css/main.css';
+
+library.add(fab)
+dom.i2svg()
